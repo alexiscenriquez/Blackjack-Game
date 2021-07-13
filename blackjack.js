104b8e2d78a1dfa03e589dccc1874e8a78455a7e
alexi <acenriqu@cougarnet.uh.edu>
2021-07-13 14:51:48 -0500

    progress

--- blackjack.js
+++ blackjack.js
@@ -1,29 +1,44 @@
+
+//console.log(sum)
+let hasBlackJack = false
+let isAlive = true;
+let message;
+let messageEl = document.getElementById("message-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.getElementById("cards-el");
 let firstCard = Math.floor(Math.random() * (11 - 2 + 1) + 2);
 let secondCard = Math.floor(Math.random() * (11 - 2 + 1) + 2);
 console.log(firstCard)
 console.log(secondCard)
 let sum = firstCard + secondCard
-console.log(sum)
-let hasBlackJack = false
-let isAlive = true;
-let message;
+let cards = [firstCard, secondCard]
+function renderGame() {
 
-function startGame() {
+
+
     if (sum <= 20) {
-        message = console.log("Do you want to draw a new card?")
+        message = "Do you want to draw a new card?"
 
     }
     else if (sum === 21) {
-        message = console.log("woohoo! You got blackjack")
+        message = "You got blackjack"
         hasBlackJack = true
     }
     else {
-        message = console.log("You're out of the game")
+        message = "You're out of the game"
         isAlive = false
     }
-
-
-    console.log(message)
-
+    messageEl.textContent = message;
+    sumEl.textContent = `Sum: ${sum}`
+    cardsEl.textContent = `Cards: ${cards[0]},${cards[1]}`
 }
 
+function startGame() {
+    renderGame();
+}
+function newGame() {
+    console.log("Drawing a new card from the deck")
+    let firstCard = Math.floor(Math.random() * (11 - 2 + 1) + 2);
+    sum += firstCard;
+    renderGame();
+}
